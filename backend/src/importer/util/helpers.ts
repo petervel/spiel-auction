c7887b65fd692ae3b39dcc02047080fdd70a31fe
Peter Vel <petervel@gmail.com>
2024-09-29 18:20:15 +0200

    removing 12th

--- backend/src/importer/util/helpers.ts
+++ backend/src/importer/util/helpers.ts
@@ -84,6 +84,7 @@ export function removeKnownContexts(text: string): string {
 	text = text.replace(/\b(\d+)\s*%/i, ""); // 12%
 	text = text.replace(/\b(\d+)\.?\s*(aug|sept|oct|okt)?/i, ""); // 12 sept, 12. Okt
 	text = text.replace(/\b(\d+)-(\d+)/i, ""); // 12-9
+	text = text.replace(/\b(\d+)(th|st|nd)?/i, ""); // 12th
 	return text;
 }
 

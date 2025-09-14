af5981330c2f439a6fc0b450ba4dc6a8001864e7
Peter Vel <petervel@gmail.com>
2025-09-14 16:58:41 +0200

    Added sorting to starred page

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ed3775ef'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.qbh4vv33vnk"
+    "revision": "0.8thrhje1tok"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
--- frontend/src/pages/LatestPage/StarredPage.tsx
+++ frontend/src/pages/LatestPage/StarredPage.tsx
@@ -1,3 +1,7 @@
+import { Euro, SortByAlpha, Timer } from '@mui/icons-material';
+import { Stack } from '@mui/material';
+import classNames from 'classnames';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Container } from '../../components/Container/Container';
 import { ItemsList } from '../../components/ItemsList/ItemsList';
@@ -6,12 +10,22 @@ import { TabBar } from '../../components/TabBar/TabBar';
 import { Title } from '../../components/Title/Title';
 import { useStarred } from '../../hooks/useStarred';
 import { useUser } from '../../hooks/useUser';
+import { SORTING, sortItems } from '../../util';
+import css from '../BidsPages/BuyingPage.module.css';
+import SortToggle from '../BidsPages/SortToggle';
 
 export const StarredPage = () => {
 	const { user, isLoading } = useUser();
 	// console.log({ user, isLoading });
 
 	const { starred } = useStarred();
+
+	const [sorting, setSorting] = useState<SORTING>(SORTING.END_DATE);
+
+	const [showSort, setShowSort] = useState(false);
+	const toggleSort = () => {
+		setShowSort((v) => !v);
+	};
 
 	const navigate = useNavigate();
 	if (isLoading) {
@@ -23,14 +37,64 @@ export const StarredPage = () => {
 	}
 
 	const items = starred ?? [];
+	const sortedItems = sortItems(items, sorting);
+
 	return (
 		<>
 			<TabBar />
 
 			<Container>
-				<Title title="Starred items" />
-				<ItemsList items={items} allowStars={true} />
+				<Title
+					title="Starred items"
+					right={<SortToggle toggleSort={toggleSort} />}
+				/>
+
+				<>
+					{showSort && (
+						<Stack direction="row" justifyContent="center">
+							<Stack
+								direction="row"
+								justifyContent="space-around"
+								my={2}
+								gap={5}
+							>
+								{SORT_BUTTONS.map((button) => (
+									<div
+										key={button.setting}
+										className={classNames({
+											[css.button]: true,
+											[css.active]:
+												button.setting == sorting,
+										})}
+										onClick={() =>
+											setSorting(button.setting)
+										}
+									>
+										{button.icon}
+									</div>
+								))}
+							</Stack>
+						</Stack>
+					)}
+
+					<ItemsList items={sortedItems} allowStars={true} />
+				</>
 			</Container>
 		</>
 	);
 };
+
+const SORT_BUTTONS = [
+	{
+		setting: SORTING.END_DATE,
+		icon: <Timer />,
+	},
+	{
+		setting: SORTING.NAME,
+		icon: <SortByAlpha />,
+	},
+	{
+		setting: SORTING.PRICE,
+		icon: <Euro />,
+	},
+];

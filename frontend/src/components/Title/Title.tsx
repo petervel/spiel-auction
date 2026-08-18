20296d6f323bf90e1b92fe8a1c832733fa6048ea
Peter Vel <petervel@gmail.com>
2026-08-18 23:44:45 +0200

    Fixed margin error

--- frontend/src/components/Title/Title.tsx
+++ frontend/src/components/Title/Title.tsx
@@ -16,6 +16,7 @@ export const Title = ({ title, left, right }: TitleProps) => {
 				variant="h5"
 				component="h2"
 				className={css.title}
+				mx={0}
 				sx={{ position: left || right ? 'absolute' : '' }}
 			>
 				{title}
